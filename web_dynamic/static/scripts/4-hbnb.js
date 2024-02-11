55daaea54c16b85a106e8d5632f9aa3b6b14d08f
Eyosiyas94 <eyosi94@gmail.com>
2024-02-11 18:05:27 +0000

    a

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,73 @@
+function appendPlace(data) {
+  $(".places").empty();
+  const places = data.toSorted((placeA, placeB) => {
+    return placeA.name.localeCompare(placeB.name);
+  });
+  for (const place of places) {
+    const element = `<article>
+                  <div class="title_box">
+                    <h2>${place.name}</h2>
+                    <div class="price_by_night">$${place.price_by_night}</div>
+                  </div>
+                  <div class="information">
+                    <div class="max_guest">${place.max_guest} Guest${
+      place.max_guest !== 1 ? "s" : ""
+    }</div>
+                    <div class="number_rooms">${place.number_rooms} Bedroom${
+      place.number_rooms !== 1 ? "s" : ""
+    }</div>
+                    <div class="number_bathrooms">${
+                      place.number_bathrooms
+                    } Bathroom${place.number_bathrooms !== 1 ? "s" : ""}</div>
+                  </div>
+                  <div class="description">
+                    ${place.description}
+                  </div>
+                </article>`;
+    $(".places").append(element);
+  }
+}
+
+$(document).ready(function () {
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:5001/api/v1/places_search",
+    contentType: "application/json",
+    data: "{}",
+    success: appendPlace,
+  });
+
+  $("button").click(function () {
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:5001/api/v1/places_search",
+      contentType: "application/json",
+      data: JSON.stringify({ amenities: Object.keys(amenitie) }),
+      success: appendPlace,
+    });
+  });
+
+  const url = http://${window.location.hostname}:5001/api/v1/status/;
+  $.get(url, function (data, status) {
+    if (data.status === "OK") {
+      $("#api_status").addClass("available");
+    } else {
+      $("#api_status").removeClass("available");
+    }
+  });
+
+  const amenitie = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(":checked")) {
+      amenitie[$(this).attr("data-id")] = $(this).attr("data-name");
+    } else {
+      delete amenitie[$(this).attr("data-id")];
+    }
+    const name = Object.values(amenitie);
+    if (name.length > 0) {
+      $(".amenities h4").text(name.join(", "));
+    } else {
+      $(".amenities h4").html("&nbsp;");
+    }
+  });
+});
